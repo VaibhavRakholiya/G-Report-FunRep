7e40bd54b45a8645b7fa5ffc78a43dd4b666c078
VaibhavRakholiya <rakholiyavaibhav.git@gmail.com>
2024-05-07 23:26:15 +0530

    Changed the port number.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3001, function () {
+app.listen(3003, function () {
   console.log('Example app listening on port 3001!');
 });
